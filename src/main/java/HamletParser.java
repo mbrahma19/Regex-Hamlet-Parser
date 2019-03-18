facb9f875c99571e99ab7cb5e4fb10468ff9d06d
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-17 23:09:15 -0400

    Completed tests and methods

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,8 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private Pattern pattern;
+    private Matcher matcher;
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -20,8 +24,9 @@ public class HamletParser {
 
         try(Scanner scanner = new Scanner(file)){
             while(scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                result.append(line).append("\n");
+                String line = findAndReplace("Horatio", "Tariq",scanner.nextLine());
+                String secondLine = findAndReplace("Hamlet", "Leon",line);
+                result.append(secondLine).append("\n");
             }
 
             scanner.close();
@@ -32,8 +37,39 @@ public class HamletParser {
         return result.toString();
     }
 
+
+    public String findAndReplace(String item, String replacement, String sentence){
+        String result;
+        if(findString(item,sentence)){
+            result = replacementString(item, replacement, sentence);
+        }else{
+            result = sentence;
+        }
+        return result;
+    }
+
+    public Boolean findString(String item, String sentence){
+        String stringPattern = ".*"+ item +".*";
+        pattern = Pattern.compile(stringPattern,Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(sentence);
+        return matcher.matches();
+    }
+
+    public String replacementString(String item, String replacement, String sentence){
+        String stringPattern = "("+ item +")";
+        pattern = Pattern.compile(stringPattern,Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(sentence);
+        String replaceAll = matcher.replaceAll(replacement);
+        return replaceAll;
+    }
+
     public String getHamletData(){
         return hamletData;
     }
+
+//    public static void main(String[] args) {
+//        HamletParser hp = new HamletParser();
+//        System.out.println(hp.getHamletData());
+//    }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,137 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //given
+        String text = "my name is Hamlet";
+        String expected = "my name is Leon";
+        String find = "HAMLET";
+        //when
+        String actual = hamletParser.replacementString(find,"Leon", text);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHamletToLeonAllCaps() {
+        //given
+        String text = "my name is Hamlet";
+        String expected = "my name is Leon";
+        String find = "hamlet";
+        //when
+        String actual = hamletParser.replacementString(find,"Leon", text);
+        //then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //given
+        String text = "my name is Horatio";
+        String expected = "my name is Tariq";
+        String find = "Horatio";
+        //when
+        String actual = hamletParser.replacementString(find,"Tariq", text);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHoratioToTariqAllCaps() {
+        //given
+        String text = "my name is HORATIO";
+        String expected = "my name is Tariq";
+        String find = "Horatio";
+        //when
+        String actual = hamletParser.replacementString(find,"Tariq", text);
+        //then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        //given
+        String text = "my name is Horatio";
+        String find = "Horatio";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHoratioAllCaps() {
+        //given
+        String text = "my name is HORATIO the one and only";
+        String find = "Horatio";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHoratioNoSpace() {
+        //given
+        String text = "my name is the one and onlyhoratio";
+        String find = "Horatio";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHoratioWhenNotThere(){
+        //given
+        String text = "my name is the Hamlet the great";
+        String find = "Horatio";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertFalse(result);
     }
 
     @Test
     public void testFindHamlet() {
+        //given
+        String text = "my name is Hamlet";
+        String find = "Hamlet";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHamletAllCaps() {
+        //given
+        String text = "HAMLET is my name";
+        String find = "Hamlet";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHamletNoSpace() {
+        //given
+        String text = "my name is the one and onlyHAMLET";
+        String find = "Hamlet";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindHamletWhenNotThere(){
+        //given
+        String text = "my name is the one and onlyhoratio";
+        String find = "Hamlet";
+        //when
+        Boolean result = hamletParser.findString(find, text);
+        //then
+        Assert.assertFalse(result);
     }
 }
